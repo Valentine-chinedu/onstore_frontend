831c55dfd5ea57462366f56ee9a952ed42d574e4
okosival <valentine11.dev@gmail.com>
2021-11-20 14:01:20 +0000

    created pages folder and exported pagesusing an index file

--- src/pages/Home/components/Accordion.jsx
+++ src/pages/Home/components/Accordion.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import DropDown from "./DropDown";
+import React from 'react';
+import DropDown from './DropDown';
 
 const Accordion = () => {
 	const accordions = [
 		{
 			_id: 1,
-			title: "What is your return policy?",
+			title: 'What is your return policy?',
 			content: `Sed laoreet dolor diam. Fusce ex lorem, consequat at tincidunt quis,
             posuere sit amet neque. Morbi efficitur purus vel consequat
             placerat. Mauris aliquet arcu tellus, eget finibus ligula vehicula
@@ -14,7 +14,7 @@ const Accordion = () => {
 
 		{
 			_id: 2,
-			title: "How much is shipping?",
+			title: 'How much is shipping?',
 			content: `Sed laoreet dolor diam. Fusce ex lorem, consequat at tincidunt quis,
             posuere sit amet neque. Morbi efficitur purus vel consequat
             placerat. Mauris aliquet arcu tellus, eget finibus ligula vehicula
@@ -23,7 +23,7 @@ const Accordion = () => {
 
 		{
 			_id: 3,
-			title: "check the status of my order?",
+			title: 'check the status of my order?',
 			content: `Sed laoreet dolor diam. Fusce ex lorem, consequat at tincidunt quis,
             posuere sit amet neqanu. Morbi efficitur purus vel consequat
             placerat. Mauris aliquet arcu tellus, eget finibus ligula vehicula
@@ -32,7 +32,7 @@ const Accordion = () => {
 
 		{
 			_id: 4,
-			title: "When will my order ship?",
+			title: 'When will my order ship?',
 			content: `Sed laoreet dolor diam. Fusce ex lorem, consequat at tincidunt quis,
             posuere sit amet neque. Morbi efficitur purus vel consequat
             placerat. Mauris aliquet arcu tellus, eget finibus ligula vehicula
@@ -40,10 +40,10 @@ const Accordion = () => {
 		},
 	];
 	return (
-		<div className="pb-4">
+		<div className='pb-4'>
 			{accordions.map((accordion) => (
 				<div
-					className="border rounded-md mx-4 pt-5 pl-6 pr-4 mb-4"
+					className='border rounded-md mx-4 pt-5 pl-6 pr-4 mb-4'
 					key={accordion._id}
 				>
 					<DropDown accordion={accordion} />
