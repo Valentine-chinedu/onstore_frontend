f1f3c54d40746b45fbbe1583f56468f3854368bf
okosival <okosival@gmail.com>
2022-11-13 17:06:25 +0100

    completed

---
+++ seeds/products.ts
@@ -0,0 +1,153 @@
+const products = [
+	{
+		name: 'Suit Trousers',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/bVhS89yZu0HOVstd|Suit_Material_Trousers.jpg',
+		description: 'Men 3 In 1 Suit Material Trousers',
+		category: 'pants',
+		price: 89.99,
+		countInStock: 3,
+		rating: 0,
+		numReviews: 0,
+	},
+	{
+		name: 'Joggers Pant',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/e5CFgt9z7CrpGN5y|Joggers_Pant.jpg',
+		description: 'Best Quality Men Black Cargo Joggers Pant',
+		category: 'pants',
+		price: 99.99,
+		countInStock: 10,
+		rating: 0,
+		numReviews: 0,
+	},
+	{
+		name: 'Orange Shirt',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/o0ZXMk8GSRTo8Zrg|Contrasting_Shirt_Orange.jpg',
+		description: 'Constrasting shirt Orange',
+		category: 'shirts',
+		price: 29.99,
+	},
+	{
+		name: 'Long sleeve shirt',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/glPiqgbQ7ePWKyYa|Design_Long_Sleeve_Shirt.jpg',
+		description: "4 In 1 Men's Classic Design Long Sleeve Shirt",
+		category: 'shirts',
+		price: 39.99,
+	},
+	{
+		name: 'Ankle Sneakers',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/txuPQOQmsHUiSh1e|Designer_Sneakers_Black.jpg',
+		description: 'Elegant Designer Athletic Ankle Sneakers',
+		category: 'shoes',
+		price: 29.99,
+	},
+	{
+		name: 'Quartz Wrist Watch',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/YrK8em77ZfMnUe9Z|Men_Military_Quartz_Brown.jpg',
+		description: "Men's Sport Military Quartz Wrist Watch-Light Brown",
+		category: 'watches',
+		price: 29.99,
+	},
+	{
+		name: 'Waterproof Watch',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/bJgmAf2KgVSv1pTO|Waterproof_MilitBlack.jpg',
+		description: "Men's Waterproof Military Watches Black",
+		category: 'watches',
+		price: 29.99,
+	},
+	{
+		name: 'Khaki Shoe Leather',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/HEE95JmdffaTSxCg|selling_image1.png',
+		description: 'Elegant Designer Khaki Shoe Leather',
+		category: ['shoes', 'top-items'],
+		price: 49.99,
+	},
+	{
+		name: 'Black Leather Shoes',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/TMe1vP1ZenbFwxNT|selling_image2.png',
+		description: 'Elegant Black Leather Dress Shoes',
+		category: ['shoes', 'top-items'],
+		price: 39.99,
+	},
+	{
+		name: 'Denim Jeans',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/CF8TbVXjrF3tYete|selling_image3.png',
+		description: 'Quality Denim pants jeans',
+		category: ['pants', 'top-items'],
+		price: 29.99,
+	},
+	{
+		name: 'Blue Jeans',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/PcyoxLmlE4OxN8YC|selling_image4.png',
+		description: 'Elegant Blue Jeans Trousers',
+		category: ['pants', 'top-items'],
+		price: 29.99,
+	},
+	{
+		name: 'Rolex Watch',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/Uw3QcTti4ODFykB4|selling_image5.png',
+		description: 'Automatic Rolex Watch',
+		category: ['watches', 'top-items'],
+		price: 49.99,
+	},
+	{
+		name: 'Apple Series 3',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/JoRqUbUknZur1xXA|selling_image6.png',
+		description: 'Apple Watch Series 3',
+		category: ['watches', 'top-items'],
+		price: 49.99,
+	},
+	{
+		name: 'strip shirt Sleeve',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/hvkJAHctsseseQU2|selling_image7.png',
+		description: 'Elegant strip shirt Sleeve',
+		category: ['shirts', 'top-items'],
+		price: 29.99,
+	},
+	{
+		name: 'Green Polo Collage',
+		image: '/images/alexa.jpg',
+		description: 'Elegant Green Polo Collage shirt',
+		category: ['shirts', 'top-items'],
+		price: 29.99,
+	},
+	{
+		name: 'T-shirt',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/JxsaKKiXFa6J9FEF|featured_image1.png',
+		description: 'Minimal T-shirt',
+		category: ['shirts', 'featured-items'],
+		price: 39.99,
+	},
+	{
+		name: 'Leather Shoes',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/0dSjpQIuoPQvwTSo|featured_image2.png',
+		description: 'Elegant Leather Shoes',
+		category: ['shoes', 'featured-items'],
+		price: 49.99,
+	},
+	{
+		name: 'Blue Jeans',
+		image:
+			'https://cdn.chec.io/merchants/22021/assets/hKIIa9bwOVuoqLgd|featured_image3.png',
+		description: 'Elegant Blue Jeans',
+		category: ['pants', 'featured-items'],
+		price: 29.99,
+	},
+];
+
+export default products;
