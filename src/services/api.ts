6f4e2922479fdaa71c9e2d20123ae1d275a33b64
Valentine-chinedu <okosival@gmail.com>
2023-01-14 21:44:54 +0100

    added headers to end points

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-	baseURL: 'process.env.REACT_APP_API_URL',
+	baseURL: 'https://api.chec.io/v1/',
+	headers: {
+		'X-Authorization': process.env.REACT_APP_CHEC_PUBLIC_KEY,
+		'Content-Type': 'application/json',
+	},
 });
--- src/services/clientApi.ts
+++ src/services/clientApi.ts
@@ -1,73 +1,93 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Carts, Categories, Products } from '../types';
 
+const headers = {
+	'X-Authorization': process.env.REACT_APP_CHEC_PUBLIC_KEY,
+};
+
 export const clientApi = createApi({
 	reducerPath: 'client',
 	tagTypes: ['carts'],
 	baseQuery: fetchBaseQuery({
 		baseUrl: 'https://api.chec.io/v1/',
-		prepareHeaders: (headers) => {
-			headers.set(
-				'Authorization',
-				`Bearer ${process.env.REACT_APP_CHEC_PUBLIC_KEY}`
-			);
-			headers.set('content-type', 'application/json');
-			return headers;
-		},
+		// prepareHeaders: (headers) => {
+		// 	headers.set(
+		// 		'X-Authorization',
+		// 		`Bearer ${process.env.REACT_APP_CHEC_PUBLIC_KEY}`
+		// 	);
+
+		// 	return headers;
+		// },
 	}),
 	endpoints: (builder) => ({
 		getProducts: builder.query<Products[], void>({
-			query: () => 'products',
+			query: () => ({
+				url: 'products',
+				headers: headers,
+			}),
+			transformResponse: (response: { data: Products[] }, meta, arg) =>
+				response.data,
 		}),
 
 		getCategories: builder.query<Categories[], void>({
-			query: () => 'categories',
+			query: () => ({ url: 'categories', headers: headers }),
+			transformResponse: (response: { data: Categories[] }, meta, arg) =>
+				response.data,
 		}),
 
-		getCart: builder.query<Carts[], void>({
-			query: () => 'carts',
+		getCart: builder.query<Carts, void>({
+			query: () => ({ url: 'carts', headers: headers }),
+
 			providesTags: [{ type: 'carts', id: 'LIST' }],
 		}),
 
-		AddToCart: builder.mutation<Carts, { data: Partial<Carts> }>({
-			query: (data) => ({
-				url: 'carts',
+		addToCart: builder.mutation<
+			Carts,
+			{ cart_id: string; product_id: string; quantity: number }
+		>({
+			query: ({ cart_id, ...data }) => ({
+				url: `carts/${cart_id}`,
 				method: 'POST',
 				body: data,
+				headers: headers,
 			}),
+
 			invalidatesTags: (result) =>
 				result ? [{ type: 'carts', id: 'LIST' }] : [],
 		}),
 
-		UpdateCartQty: builder.mutation<
+		updateCartQty: builder.mutation<
 			Carts,
 			{ cart_id: string; line_item_id: string; quantity: number }
 		>({
 			query: ({ cart_id, line_item_id, quantity }) => ({
 				url: `carts/${cart_id}/${line_item_id}`,
-				method: 'PUT',
+				method: 'POST',
 				body: quantity,
+				headers: headers,
 			}),
 			invalidatesTags: (result) =>
 				result ? [{ type: 'carts', id: 'LIST' }] : [],
 		}),
 
-		RemoveFromCart: builder.mutation<
+		removeFromCart: builder.mutation<
 			Carts,
 			{ cart_id: string; line_item_id: string }
 		>({
 			query: ({ cart_id, line_item_id }) => ({
 				url: `carts/${cart_id}/items/${line_item_id}`,
-				method: 'DELETE',
+				method: 'POST',
+				headers: headers,
 			}),
 			invalidatesTags: (result) =>
 				result ? [{ type: 'carts', id: 'LIST' }] : [],
 		}),
 
 		emptyCart: builder.mutation<Carts, { cart_id: string }>({
-			query: ({ cart_id }) => ({
+			query: (cart_id) => ({
 				url: `carts/${cart_id}/items`,
 				method: 'DELETE',
+				headers: headers,
 			}),
 			invalidatesTags: (result) =>
 				result ? [{ type: 'carts', id: 'LIST' }] : [],
