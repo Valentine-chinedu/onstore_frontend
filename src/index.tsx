14cd04d9671c52a0a553908d432bfe2901bd0185
Valentine-chinedu <okosival@gmail.com>
2023-01-14 21:37:26 +0100

    imported store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from './services/store';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
