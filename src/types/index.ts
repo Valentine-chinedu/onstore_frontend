f79c7e989fb3d4a151fdd0ffb63dbb0c5b03321d
Valentine-chinedu <okosival@gmail.com>
2023-01-14 21:45:15 +0100

    del oders

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,4 @@
 export type { Carts } from './carts';
 export type { Categories } from './categories';
 export type { Products } from './products';
-export type { Order } from './order';
 export type { Params } from './searchParams';
-export type { ProductsContextState } from './productsContext';
