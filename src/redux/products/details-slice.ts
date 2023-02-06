433f125ec1c19f581db0c7217ce84b915b402ce3
Valentine-chinedu <okosival@gmail.com>
2023-02-06 16:46:54 +0100

    products slices completed and added to store

---
+++ src/redux/products/details-slice.ts
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { toast } from 'react-hot-toast';
+import { Product } from '../../types';
+import { setError } from '../../utils/error';
+import publicAxios from '../../utils/public-axios';
+
+export interface ProductSliceState {
+	product: Product | null;
+	loading: boolean;
+	error: null | object;
+}
+
+const initialState: ProductSliceState = {
+	product: null,
+	loading: false,
+	error: null,
+};
+
+export const getProductById = createAsyncThunk(
+	'products/details',
+	async (id: string) => {
+		try {
+			const res = await publicAxios.get(`/products/${id}`);
+			if (res.data) {
+				return res.data;
+			}
+		} catch (error: any) {
+			const message = setError(error);
+			toast.error(message);
+		}
+	}
+);
+
+export const productDetailsSlice = createSlice({
+	name: 'product-detail',
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder.addCase(getProductById.pending, (state) => {
+			// Add user to the state array
+			state.loading = true;
+		});
+		builder.addCase(getProductById.fulfilled, (state, action) => {
+			state.loading = false;
+			state.product = action.payload;
+		});
+		builder.addCase(
+			getProductById.rejected,
+			(state, action: PayloadAction<any>) => {
+				state.loading = false;
+				state.error = action.payload;
+			}
+		);
+	},
+});
+
+// Action creators are generated for each case reducer function
+
+export default productDetailsSlice;
