11469085e0fafbf0506302fdc6453f94b4650ac6
Valentine-chinedu <okosival@gmail.com>
2023-02-06 16:37:53 +0100

    feat: created a dynamic route for categories

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
 import { Footer, Header } from './components';
-import AllProducts from './components/Categories.tsx/AllProducts';
-import Pants from './components/Categories.tsx/Pants';
-import Shirts from './components/Categories.tsx/Shirts';
-import Shoes from './components/Categories.tsx/Shoes';
-import Watches from './components/Categories.tsx/Watches';
+// import AllProducts from './components/Categories.tsx/AllProducts';
+import Products from './components/Categories.tsx/Products';
+// import Shirts from './components/Categories.tsx/Shirts';
+// import Shoes from './components/Categories.tsx/Shoes';
+// import Watches from './components/Categories.tsx/Watches';
 import { Cart, Home, ProductDetails } from './pages';
 import Categories from './pages/Categories';
 
@@ -20,12 +20,7 @@ const App = () => {
 				<Route index element={<Home />} />
 				<Route path='home' element={<Home />} />
 				<Route path='categories' element={<Categories />}>
-					<Route index element={<Pants />} />
-					<Route path='pants' element={<Pants />} />
-					<Route path='shirts' element={<Shirts />} />
-					<Route path='shoes' element={<Shoes />} />
-					<Route path='watches' element={<Watches />} />
-					<Route path='allproducts' element={<AllProducts />} />
+					<Route path='/category/:category' element={<Products />} />
 				</Route>
 				<Route path='/home/product/:id' element={<ProductDetails />} />
 				<Route path='cart' element={<Cart />} />
--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,40 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { getCategories } from '../redux/categories/slice-list';
+import { useAppDispatch, useAppSelector } from '../redux/store';
 
 const Categories = () => {
+	const { categories } = useAppSelector((state) => state.categoriesList);
+	const dispatch = useAppDispatch();
+
+	useEffect(() => {
+		dispatch(getCategories());
+	}, [dispatch]);
 	return (
 		<div className='flex-col items-center md:flex'>
 			<nav className='flex h-16 w-full items-end justify-center space-x-8 bg-gray-600 pb-2 text-sm font-medium text-gray-100'>
-				<Link
-					to='pants'
-					className=':text-gray-300 focuse:text-gray-400 hover:text-gray-400'
-				>
-					Pants
-				</Link>
-				<Link
-					to='shirts'
-					className=':text-gray-300 focuse:text-gray-400 hover:text-gray-400'
-				>
-					Shirts
-				</Link>
-				<Link
-					to='shoes'
-					className=':text-gray-300 focuse:text-gray-400 hover:text-gray-400'
-				>
-					Shoes
-				</Link>
-				<Link
-					to='watches'
-					className=':text-gray-300 focuse:text-gray-400 hover:text-gray-400'
-				>
-					Watches
-				</Link>
-				<Link
-					to='allproducts'
-					className=':text-gray-300 focuse:text-gray-400 hover:text-gray-400'
-				>
-					All Products
-				</Link>
+				{categories?.map((category) => (
+					<Link
+						to={`/category/${category}`}
+						className='focuse:text-gray-400 text-gray-300 hover:text-gray-400'
+					>
+						{category}
+					</Link>
+				))}
 			</nav>
 			<div className='md:w-8/12'>
 				<Outlet />
