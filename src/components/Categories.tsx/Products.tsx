001741d68d75a3c96364ebd242d8660aa0729c3d
Valentine-chinedu <okosival@gmail.com>
2023-02-06 16:41:04 +0100

    feat: product component for all categories

---
+++ src/components/Categories.tsx/Products.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import { FaSpinner } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
+import { getProductByCategory } from '../../redux/products/category-slice';
+import { useAppDispatch, useAppSelector } from '../../redux/store';
+
+import ProductDisplay from '../products/ProductDisplay';
+
+const Products = () => {
+	const { products: items, loading } = useAppSelector(
+		(state) => state.productsByCategory
+	);
+
+	const dispatch = useAppDispatch();
+	const params = useParams()
+	const category = params.category
+
+	useEffect(() => {
+		dispatch(getProductByCategory(category!));
+	}, [dispatch, category]);
+
+	if (loading) {
+		return (
+			<div className='flex h-screen flex-col items-center justify-center'>
+				<FaSpinner size={20} className='mb-4 animate-spin' />
+			</div>
+		);
+	}
+	return (
+		<div className='mx-4 grid h-full grid-cols-2 gap-x-4 gap-y-6 py-8 md:mx-0 md:grid-cols-4 md:gap-x-8 md:gap-y-12'>
+			{items?.map((item) => (
+				<div className='' key={item._id}>
+					<ProductDisplay product={item} />
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Products;
