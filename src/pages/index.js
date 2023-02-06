c4a7e5d30d94bb8fc211a4cfe72fc37c92a7aa9f
Valentine-chinedu <okosival@gmail.com>
2023-02-06 16:33:52 +0100

    deletd

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Products } from '../components/Categories.tsx/AllProducts';
-export { default as ProductDetails } from '../components/products/ProductDetails';
-export { default as Home } from './Home/Home';
-export { default as Cart } from './Cart';
--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const apiClient = axios.create({
-	baseURL: 'https://api.chec.io/v1/',
-	headers: {
-		'X-Authorization': process.env.REACT_APP_CHEC_PUBLIC_KEY,
-		'Content-Type': 'application/json',
-	},
-});
