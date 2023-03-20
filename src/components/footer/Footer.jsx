a4b2a0973fc0360dc86c72ac5cc315b78618e9e6
Valentine-chinedu <okosival@gmail.com>
2023-03-20 11:10:41 +0100

    fix: remove react hook form

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AiOutlineSend } from 'react-icons/ai';
-import { useForm } from 'react-hook-form';
 
 const Footer = () => {
-	const { register, handleSubmit, errors, reset } = useForm();
-
-	const onSubmit = (data) => {
-		console.log(data);
-		reset();
-	};
-
 	return (
-		<div className='flex-col items-center justify-center bg-[#000000] py-8 md:flex'>
-			<div className=' flex w-full flex-col items-center space-y-12 md:w-8/12 md:flex-row md:items-center md:justify-evenly md:space-y-0 md:space-x-4 md:py-10 md:pl-0'>
+		<div className=' flex-col items-center justify-center bg-[#000000] py-8 md:flex'>
+			<div className='flex w-full flex-col items-center space-y-12 md:w-8/12 md:flex-row md:items-center md:justify-evenly md:space-y-0 md:space-x-4 md:py-10 md:pl-0'>
 				<div className='ml-8 flex w-full items-center justify-between'>
 					<div className='space-y-6'>
 						<h2 className=' font-medium text-gray-100'>SHOP</h2>
 
-						<ul className='flex-row text-sm text-gray-300'>
+						<ul className='flex-row text-sm text-gray-300 md:text-base'>
 							<Link to='/categories/allproducts'>
-								<li className='mr-8 mb-6 hover:cursor-pointer hover:text-gray-100 md:mr-24 md:w-20'>
+								<li className='mr-8 mb-6 hover:cursor-pointer hover:text-gray-100 md:mr-20 md:w-24'>
 									All Products
 								</li>
 							</Link>
@@ -42,7 +34,7 @@ const Footer = () => {
 					<div className='space-y-6'>
 						<h2 className='font-medium text-gray-100 '>PAGES</h2>
 
-						<ul className='flex-row text-sm text-gray-300'>
+						<ul className='flex-row text-sm text-gray-300 md:text-base'>
 							<li className='mr-8 mb-6 hover:cursor-pointer hover:text-gray-100 lg:mr-24'>
 								<Link to='/home'>Home</Link>
 							</li>
@@ -63,7 +55,7 @@ const Footer = () => {
 					<div className='space-y-6'>
 						<h2 className=' font-medium text-gray-100'>INFORMATION</h2>
 
-						<ul className='flex-row text-sm text-gray-300'>
+						<ul className='flex-row text-sm text-gray-300 md:text-base'>
 							<li className='mr-8 mb-6 hover:cursor-pointer hover:text-gray-100 md:mr-24'>
 								About us
 							</li>
@@ -86,8 +78,8 @@ const Footer = () => {
 					<div className=' mb-1.5 flex flex-col'>
 						<div className=' flex flex-col items-center '>
 							<div className='space-x-1 text-xs'>
-								<span className='text-gray-50'>Get your</span>
-								<span className='text-red-500'>10% off</span>
+								<span className='text-gray-50 md:text-sm'>Get your</span>
+								<span className='text-red-500 md:text-sm'>10% off</span>
 							</div>
 							<h1 className='mb-4 text-sm font-medium text-gray-300 md:text-base'>
 								Join Our Mailing List
@@ -95,12 +87,7 @@ const Footer = () => {
 						</div>
 
 						<div>
-							<form onSubmit={handleSubmit(onSubmit)}>
-								{errors.email && (
-									<p className='ml-4 mb-2 text-red-600'>
-										{errors.email.message}
-									</p>
-								)}
+							<form>
 								<div className='relative flex h-10 w-64 items-center rounded-3xl bg-gray-200 pl-4 md:w-80'>
 									<h2 className='mr-2 text-xl text-gray-400'>@</h2>
 									<input
@@ -108,13 +95,6 @@ const Footer = () => {
 										type='text'
 										name='email'
 										placeholder='Email address'
-										ref={register({
-											required: 'Email is required.',
-											pattern: {
-												value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-												message: 'Email is not valid.',
-											},
-										})}
 									/>
 									<button
 										className='absolute left-56 rounded-full bg-[#FFA500] p-2.5 hover:bg-orange-800 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 md:left-72'
@@ -127,10 +107,12 @@ const Footer = () => {
 						</div>
 					</div>
 					<div className='flex items-center'>
-						<p className='mr-1 text-xs text-gray-300'>
+						<p className='mr-1 text-xs text-gray-300 md:text-base'>
 							We never share your info. View our
 						</p>
-						<p className='text-xs font-medium text-gray-50'>Privacy Policy</p>
+						<p className='text-xs font-medium text-gray-50 md:text-base'>
+							Privacy Policy
+						</p>
 					</div>
 				</div>
 			</div>
