a89191fd035b7f1a89eba72839a4bfb2d39028f3
Valentine-chinedu <okosival@gmail.com>
2023-03-27 11:43:38 +0100

    optimised for mobile view

--- src/components/userPage/header/Header.tsx
+++ src/components/userPage/header/Header.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { GoX } from 'react-icons/go';
+import { FaCaretDown, FaCaretUp, FaRegUserCircle } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
 import { reset } from '../../../redux/cart/cart-Slice';
-import { getCategories } from '../../../redux/categories/slice-list';
 import { useAppDispatch, useAppSelector } from '../../../redux/store';
 import { userLogout } from '../../../redux/users/login-slice';
 import logo from './onStore_logo.png';
@@ -11,14 +11,9 @@ import logo from './onStore_logo.png';
 const Header = () => {
 	const { cartItems } = useAppSelector((store) => store.cart);
 	const { userInfo } = useAppSelector((state) => state.login);
-	const { categories } = useAppSelector((state) => state.categoriesList);
-
-	const dispatch = useAppDispatch();
-
-	const filteredCategory = categories?.filter(
-		(category) => category !== 'featured-item' && category !== 'top-selling'
-	);
 	const [isOpen, setOpen] = useState(false);
+	const [collapseUser, setCollapseUser] = useState(false);
+	const dispatch = useAppDispatch();
 
 	let navigate = useNavigate();
 
@@ -35,16 +30,13 @@ const Header = () => {
 	function handleClickToLogout() {
 		dispatch(userLogout());
 		dispatch(reset());
-		navigate('/login');
+		navigate('/home');
+		setCollapseUser(false);
 	}
 
-	useEffect(() => {
-		dispatch(getCategories());
-	}, [dispatch]);
-
 	return (
-		<nav className='md:p-o sticky  top-0 z-50 flex justify-center overflow-hidden bg-black'>
-			<div className='z-50 flex h-12 w-full items-center justify-between px-2 md:w-11/12 md:px-0 lg:h-12'>
+		<nav className='md:p-o fixed top-0 z-50 flex w-full justify-center overflow-hidden bg-black'>
+			<div className='z-50 flex h-12 w-full items-center justify-between px-2 md:w-11/12 md:px-0 lg:h-16'>
 				<div className='relative flex items-center md:mr-4 md:ml-2 lg:mr-0 lg:ml-0 lg:hidden'>
 					<button
 						onClick={() => {
@@ -78,59 +70,60 @@ const Header = () => {
 						<GoX size={25} />
 					</button>
 				</div>
-				<div className='flex items-center space-x-4 lg:mr-10'>
+				<div className='flex justify-center lg:mr-10'>
 					<button onClick={handleClickToHome}>
 						<img
-							className='ml-2 h-16 lg:h-16'
+							className='ml-2 h-20 lg:h-28'
 							src={logo}
 							alt='logo'
 							loading='lazy'
 						/>
 					</button>
-					<Link className='nav-btn' to={`/profile/${userInfo?._id}`}>
-						Profile
-					</Link>
-					{userInfo?.isAdmin && (
-						<Link className='nav-btn' to='/dashboard'>
-							Dashboard
-						</Link>
-					)}
 				</div>
 
 				<div
-					className={`fixed top-0 left-0  flex h-72 flex-col justify-center bg-gray-800 md:h-72 lg:relative lg:h-32 lg:w-auto lg:flex-row lg:items-center lg:bg-black ${
+					className={`fixed top-0 left-0 flex h-screen flex-col bg-gray-800 pt-16 md:h-72 lg:relative lg:h-32 lg:w-3/12 lg:flex-row lg:items-center lg:bg-black ${
 						isOpen
-							? 'transition-height w-40 duration-500 ease-in-out md:w-52 lg:transition-none'
+							? 'transition-height w-52 duration-500 ease-in-out lg:transition-none'
 							: 'transition-height w-0 overflow-hidden duration-500 ease-in-out lg:transition-none'
 					}`}
 				>
-					<div className='flex w-full flex-col items-start overflow-hidden lg:ml-0 lg:h-full lg:flex-row lg:items-center lg:space-x-8'>
-						<nav className='flex h-16 w-full items-center justify-center space-x-8 pb-2'>
-							{filteredCategory?.map((category) => (
-								<NavLink
-									to={`/products/${category}`}
-									onClick={() => {
-										setOpen(false);
-									}}
-									className='nav-btn'
-								>
-									{category}
-								</NavLink>
-							))}
+					<div className='flex w-full flex-col items-start space-y-8 overflow-hidden lg:ml-0 lg:h-28 lg:flex-row lg:space-y-0 lg:space-x-8'>
+						<nav
+							className='flex w-full flex-col items-start justify-center border-b-2 pl-2 pb-4 lg:w-96 lg:flex-row lg:justify-start lg:space-x-8 lg:border-0 lg:pl-0 lg:pb-0 
+								'
+						>
+							<NavLink to='/home' className='nav-btn'>
+								Home
+							</NavLink>
+							<NavLink to='/products/pants' className='nav-btn'>
+								Products
+							</NavLink>
+							<div className='nav-btn'>Contacts</div>
+							<div className='nav-btn'>About</div>
 						</nav>
+						<div className='flex flex-col items-start pl-2 lg:hidden'>
+							{userInfo?.isAdmin && (
+								<Link className='nav-btn' to='/dashboard'>
+									Dashboard
+								</Link>
+							)}
+							<Link className='nav-btn' to={`/profile/${userInfo?._id}`}>
+								Profile
+							</Link>
+							{!userInfo ? (
+								<button onClick={handleClickToLogin} className='nav-btn'>
+									Login
+								</button>
+							) : (
+								<button onClick={handleClickToLogout} className='nav-btn'>
+									Logout
+								</button>
+							)}
+						</div>
 					</div>
 				</div>
-
-				<div className='relative flex h-10 items-center space-x-6 md:mr-2 lg:mr-0'>
-					{!userInfo ? (
-						<button onClick={handleClickToLogin} className='nav-btn'>
-							Login
-						</button>
-					) : (
-						<button onClick={handleClickToLogout} className='nav-btn'>
-							Logout
-						</button>
-					)}
+				<div className='flex items-center space-x-2 md:space-x-8'>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						viewBox='0 0 24 24'
@@ -143,7 +136,39 @@ const Header = () => {
 							fill='rgba(255,255,255,1)'
 						/>
 					</svg>
-					<div className='relative flex h-10 items-center justify-around text-gray-100 md:mr-2 lg:mr-0'>
+					<div className='relative flex h-10 items-center justify-around space-x-0 text-gray-100 md:mr-2 lg:mr-0 lg:space-x-4'>
+						<div className='relative hidden h-10 items-center space-x-6 md:mr-2 lg:mr-0 lg:flex'>
+							<button
+								onClick={() => setCollapseUser((prev) => !prev)}
+								className='flex items-center space-x-1.5 text-gray-100'
+							>
+								<FaRegUserCircle size={24} />
+								{collapseUser ? <FaCaretUp /> : <FaCaretDown />}
+							</button>
+							<div
+								className={`transition-height top-16 right-20 flex h-0 flex-col items-start space-y-2 overflow-hidden bg-gray-900 duration-500 lg:fixed lg:z-50 ${
+									collapseUser && 'h-36 px-2'
+								}`}
+							>
+								{!userInfo ? (
+									<button onClick={handleClickToLogin} className='nav-btn'>
+										Login
+									</button>
+								) : (
+									<button onClick={handleClickToLogout} className='nav-btn'>
+										Logout
+									</button>
+								)}
+								<Link className='nav-btn' to={`/profile/${userInfo?._id}`}>
+									Profile
+								</Link>
+								{userInfo?.isAdmin && (
+									<Link className='nav-btn' to='/dashboard'>
+										Dashboard
+									</Link>
+								)}
+							</div>
+						</div>
 						<Link to='/cart'>
 							<svg
 								xmlns='http://www.w3.org/2000/svg'
