cce3a614b24c3a6d50c3f728e5925b29f94baa15
Valentine-chinedu <okosival@gmail.com>
2023-03-27 11:50:56 +0100

    feat: moved category navigation to products

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import DefaultLayout from '../components/layouts/DefaultLayout';
+import { getCategories } from '../redux/categories/slice-list';
+import { useAppDispatch, useAppSelector } from '../redux/store';
 
 const ProductsPage = () => {
+	const { categories } = useAppSelector((state) => state.categoriesList);
+	const filteredCategory = categories?.filter(
+		(category) => category !== 'featured-item' && category !== 'top-selling'
+	);
+	const dispatch = useAppDispatch();
+	useEffect(() => {
+		dispatch(getCategories());
+	}, [dispatch]);
 	return (
 		<DefaultLayout>
-			<div className='flex h-full flex-col items-center justify-center'>
-				<div className='md:w-8/12'>
+			<div className=''>
+				<div className='flex w-full items-center justify-center space-x-4 bg-gray-500 lg:space-x-8'>
+					{filteredCategory?.map((category) => (
+						<NavLink to={`/products/${category}`} className='nav-btn'>
+							{category}
+						</NavLink>
+					))}
+				</div>
+				<div className='flex h-full flex-col items-center py-8 md:container md:mx-auto'>
 					<Outlet />
 				</div>
 			</div>
